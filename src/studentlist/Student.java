4530f1bc46f1e8834bbbf6f26463d07647c34fb9
Harjeet Singh <sin12460@sheridancollege.ca>
2023-05-16 09:59:27 -0400

    First Commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,34 @@
+package studentlist;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Student {
+    private String sname;
+    private String sID;
+
+    public Student(String sname, String sID) {
+        this.sname = sname;
+        this.sID = sID;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+    
+    
+    
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,22 @@
+package studentlist;
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class StudentList {
+
+    public static void main(String[] args) {
+        Student s1= new Student("Harjeet","s1");
+        Student[] studentList=new Student[3];
+        studentList[0] = s1;
+        studentList[1]= new Student("abc","s2");
+        studentList[2]= new Student("def","s3");
+        
+        for (int i=0;i<studentList.length;i++) {
+            System.out.println(studentList[i].getSname());
+        }
+    }
+    
+}
