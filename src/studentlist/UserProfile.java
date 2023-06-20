68c2cda7c78d6bc3cf3351a2bde9433e28d1749e
Harjeet Singh <133786576+har1313@users.noreply.github.com>
2023-06-20 10:32:44 -0400

    Update UserProfile.java
    added comment

--- src/studentlist/UserProfile.java
+++ src/studentlist/UserProfile.java
@@ -1,3 +1,5 @@
+
+//to fetch and merge
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -25,4 +27,4 @@ class UserProfile {
         return favoriteGenre;
     }
 }
-// adding comments to fetch and merge....
+
