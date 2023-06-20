e18008c4720ff5bad63a1f464961e03dd3acbf10
Harjeet Singh <133786576+har1313@users.noreply.github.com>
2023-06-20 10:40:05 -0400

    Update UserProfile.java

--- src/studentlist/UserProfile.java
+++ src/studentlist/UserProfile.java
@@ -1,5 +1,5 @@
 
-//to fetch and merge
+//to fetch and merge according to the question.
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
